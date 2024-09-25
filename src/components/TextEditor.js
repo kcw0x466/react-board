11773f3474be057634b8ffca7cb25cf1f83ffde3
kcw0x466 <kcw774707@gmail.com>
2024-09-25 21:45:44 +0900

    Update TextEditor.js

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -66,18 +66,48 @@ function TextEditor(props) {
             alert("비밀번호를 입력해주세요.");
             return;
         }
+    
+        // 비밀번호가 포함된 새로운 포스트 객체 생성
+        const newPost = { ...Post, password: InputPassword };
+        
+        axios.post(`http://localhost:8080/api/posts`, newPost, {
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+        })
+        .then(() => {
+            console.log(newPost); // 새로운 포스트 객체 로그
+            goToHome();
+        })
+        .catch((error) => {
+            console.error("포스트 업로드 중 오류:", error);
+        });
+    };
+    
+    // const uploadPost = () => {
+    //     if (InputPassword.length < 1) {
+    //         alert("비밀번호를 입력해주세요.");
+    //         return;
+    //     }
 
-        setPost({...Post, "password": InputPassword});
-        axios.post(`http://localhost:8080/api/posts`, Post,{
-            headers: { 'content-type': 'application/json' }
-        });  
-        console.log(Post);
-        goToHome();
+    //     setPost({...Post, "password": InputPassword});
+    //     axios.post(`http://localhost:8080/api/posts`, Post,{
+    //         headers: { 'content-type': 'application/json' }
+    //     });  
+    //     console.log(Post);
+    //     goToHome();
+    // };
+
+    const updatePost = () => {
+        axios.put(`http://localhost:8080/api/posts/${params.id}`, Post, {
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+        });
+        navigate(`/Post/${params.id}`);
     };
 
-    const updatePost = () => {};
-
-    const deletePost = () => {};
+    // 백엔드 서버에서 에러남
+    const deletePost = () => {
+        axios.delete(`http://localhost:8080/api/posts/${params.id}`);
+        goToHome();
+    };
 
     const chkPassword = () => {
         if (InputPassword.length < 1) {
@@ -129,7 +159,7 @@ function TextEditor(props) {
         <Form>
             <Row>
                 <Col>
-                    <Form.Control placeholder="작성자" readOnly={OnlyRead} value={Post.nickname} onChange={handleInputNickname} />
+                    <Form.Control placeholder="작성자" readOnly={!(props.mode == "write")} value={Post.nickname} onChange={handleInputNickname} />
                 </Col>
                 <Col>
                     <Form.Control type="password" placeholder="비밀번호" onChange={handleInputPassword} />
@@ -158,11 +188,11 @@ function TextEditor(props) {
                     </Button>
                 </Col>
                 <Col style={{textAlign: "right"}}>
-                    {EnableDel && (<Button variant="danger" type="button">
+                    {EnableDel && (<Button variant="danger" type="button" onClick={deletePost}>
                         삭제
                     </Button>)}
                     {' '}
-                    {EnableEdit && (<Button variant="info" type="button">
+                    {EnableEdit && (<Button variant="info" type="button" onClick={updatePost}>
                         수정
                     </Button>)}
                     {' '}
